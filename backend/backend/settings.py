c52820d84c2ba2be27abab19a1cb10a3a193f061
Ajay <ajaydammalapati30@gmail.com>
2026-07-02 12:30:09 +0530

    WeatherAI - PM Accelerator Full Stack Assessment by Ajay Dammalapati

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,7 +35,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -60,10 +61,12 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
 WEATHER_API_KEY = '981a5555c09208e590a8979f7a934812'
 WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5'
+
+# Static files
+STATIC_URL = '/static/'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
@@ -31,12 +15,11 @@ def homepage(request):
             "export_json": "/api/weather/searches/export_json/",
             "export_csv": "/api/weather/searches/export_csv/",
             "export_xml": "/api/weather/searches/export_xml/",
-            "admin": "/admin/"
         }
     })
 
 urlpatterns = [
-    path('', homepage, name='homepage'),
+    path('', homepage),
     path('admin/', admin.site.urls),
     path('api/weather/', include('weather.urls')),
 ]
